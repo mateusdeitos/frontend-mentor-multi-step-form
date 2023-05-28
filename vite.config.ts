1f3b5fa35423ce92d292e8828224ab7982c5e1ee
Mateus Deitos <mate.deitos@hotmail.com>
2023-05-28 10:59:29 -0300

    fixes for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend-mentor-multi-step-form/",
   plugins: [react()],
   css: {
 	modules: {
